bcb4fed6247cff45d62ac83d9c769d55c1b48bbd
Chris Hartjes <chartjes@littlehart.net>
2018-03-07 16:50:50 -0500

    Update with new tests to be done during deployments

--- conftest.py
+++ conftest.py
@@ -15,21 +15,7 @@ def rel_num():
     gbl.release_num = ''
 
 
-@pytest.fixture(scope='session')
-def ticket_num():
-    """Returns the ticket number"""
-    # config = request.config
-    # return config.getoption('ticket_num')
-    gbl.ticket_num = '12345668'
-
-
 def pytest_addoption(parser):
-    parser.addini('ticket_num', help='Ticket number')
-    parser.addoption(
-        '--ticket-num',
-        metavar='ticket',
-        default=os.getenv('TICKET_NUM', None),
-        help='Ticket number the tests should use')
     parser.addoption(
         "--env",
         dest="env",
--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,22 +1,10 @@
 import requests
 
-
-def test_health(api_version, conf, env):
-    response = requests.get(conf.get(env, 'server_url') + "/health")
-    router = conf.get(env, 'router')
-    data = response.json()
-
-    assert 'version' in data
-    assert 'clients' in data
-
-    if api_version != '0.0.0':
-        assert api_version == data['version']
-
-    assert data[router]['status'] == 'OK'
-    assert data['storage']['status'] == 'OK'
+from pytest_testrail.plugin import pytestrail
 
 
-def test_status(api_version, conf, env):
+@pytestrail.case('C10691')
+def test_status_endpoint(api_version, conf, env):
     response = requests.get(conf.get(env, 'server_url') + "/status")
     data = response.json()
     assert 'status' in data
