0a6e913921b635e5140fa48817beb1f69dba45e1
Chris Hartjes <chartjes@littlehart.net>
2019-10-30 12:21:39 -0400

    Removed debugging statement

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -16,7 +16,6 @@ def test_status_endpoint(api_version, conf, env):
 def test_rs_status_endpoint(rs_api_version, conf, env):
     response = requests.get(conf.get(env, "rs_server_url") + "/__version__")
     data = json.loads(response.json())
-    print(data)
 
     if rs_api_version != "0.0.0":
         assert rs_api_version == data["version"]
